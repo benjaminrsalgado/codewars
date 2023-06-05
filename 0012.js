4d68eb588b3e0cc52b1daa550dc42c7d07114b52
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-06-05 15:53:19 -0600

    i did loops

---
+++ 0012.js
@@ -0,0 +1,16 @@
+let number = 20;
+while (number <= 40){
+    console.log(number); number ++;
+}
+
+let dar;
+for (dar = 50; dar <= 60; dar ++){
+    console.log(dar);
+}
+
+
+const saboresDeCaramelos = ['fresa', 'manzana', 'limón', 'uva'];
+
+for (const sabor of saboresDeCaramelos) {
+    console.log(sabor);
+}

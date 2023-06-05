a9df2d0f83f5b8326026b08f42e45f42945203b5
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-06-05 15:15:07 -0600

    i did conditionals

---
+++ 0011.js
@@ -0,0 +1,21 @@
+let userName = 'Ta',
+age = 32,
+luckyNumber = 13,
+albums = 10
+
+if (userName === 'Taylor' ){
+    console.log('hi buddy ');
+}else if ( age <= 32){
+    console.log('you are in')
+}else if (age >= 32){
+    console.log('you are not the person')
+}else if (luckyNumber <= 13){
+    console.log('you will have a good life')
+}else if (albums <= 10){
+    console.log(`you will be rich with your ${albums}`)
+}
+
+// i am doing a ternary operation just por practicing 
+
+//userName ? console.log('your own kid');
+//age ? console.log('')

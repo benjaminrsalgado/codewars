c21bda6eda83adf61541314f33e7258297f0def1
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-06-07 16:02:21 -0600

    i add functions

---
+++ 0014.js
@@ -0,0 +1,10 @@
+function reverseList(list) {
+    return list.reverse()
+
+}
+
+const stringToNumber = function(str){
+    let fail = Number(str)
+    return fail;
+}
+stringToNumber ("8")
---
+++ 0015.js
@@ -0,0 +1,9 @@
+function sumnumber (a, b,){ 
+    let sum = (a + b)
+    a = 20
+    console.log(sum, a) 
+}
+sumnumber (4,2, 6)
+sumnumber (5,1, 8)
+
+

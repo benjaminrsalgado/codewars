4af1380155b597b8475b3a7d24f90551a782bc62
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-06-09 12:51:37 -0600

    i practice functions and variables

---
+++ 0016.js
@@ -0,0 +1,31 @@
+//convertir de centimetros a pulgadas
+
+let centimetros = 10
+
+let conver = centimetros * 0.39
+
+console.log(conver)
+
+// segundo ejercicio 
+
+let ageBen = 20
+
+let ageGus = 39
+
+let promedio = (ageBen + ageGus)  /2
+
+console.log(promedio)
+
+// tercero
+
+let ageBenja = 20
+
+let ageMax = 39
+
+let ageFani = 28
+
+let ageAlba = 60
+
+let prom = (ageBenja + ageMax + ageFani + ageAlba)  /4
+
+console.log(prom)
---
+++ 0017.js
@@ -0,0 +1,22 @@
+//functions
+
+//acepte 4 numeros y te saque el promedio 
+
+function promedio (a, b, c, d){
+    let sum = ( a + b + c + d) /4 
+        console.log(sum)
+    
+}
+
+promedio (10, 12, 50, 23)
+
+
+// another function
+
+//Acept a name and see hi with the name of the person
+
+function nombres (name){
+    console.log( `hi ${name}`)
+}
+
+nombres ('benja')

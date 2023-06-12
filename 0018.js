50f7b7e89d20bcef5092ec57ad9fbe004a2c7cd6
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-06-12 12:38:12 -0600

    i did arrays

---
+++ 0018.js
@@ -0,0 +1,30 @@
+let names = ["durazno", "pepino", "manzana", "guayaba", "sandia", "melon", "platano", "mango", "uvas"]
+console.log(names.length)
+console.log(names[names.length -1])
+console.log(names[4])
+names.reverse()
+console.log(names)
+names.pop()
+names.push("jicama")
+console.log(names [names.length -1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

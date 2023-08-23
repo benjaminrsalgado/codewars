1a9e2242c7b3edfff59d0560618c2d8424e8bd32
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-08-23 15:16:49 -0600

    i did two excersises

---
+++ 0025.js
@@ -0,0 +1,8 @@
+function makeUpperCase(str) {
+    const makeUpperCase = str => str.toUpperCase();
+}
+
+/* 
+1.There is a function call makeUpperCase with a parameter (str) espera que el usuario ponga una cadena de palabras
+
+2.se declara una constante también llamada makeUpperCase. Esta constante tiene asignada una función de flecha (str => str.toUpperCase()). Esta función de flecha toma un parámetro str y aplica toUpperCase() a la cadena para convertirla en mayúsculas. Sin embargo, esta función de flecha se declara y se define pero nunca se utiliza.
---
+++ 0026.js
@@ -0,0 +1,8 @@
+function reverseList(list) {
+    list.reverse()
+    return (list)
+    }
+
+/* 
+1. se utilliza el metodo reverse, antes de poner el metodo se debe de poner a cual objeto se le va a aplicar en este caso seria list 
+2. return con el objeto que se le aplico anteriormente, ahora todoo lo que ponga el usuario se vera al reves

3f3d72ac22cb7076d019c353b2173418c684758b
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-08-25 23:28:00 -0600

    i make comments in the code

--- 0026.js
+++ 0026.js
@@ -6,3 +6,5 @@ function reverseList(list) {
 /* 
 1. se utilliza el metodo reverse, antes de poner el metodo se debe de poner a cual objeto se le va a aplicar en este caso seria list 
 2. return con el objeto que se le aplico anteriormente, ahora todoo lo que ponga el usuario se vera al reves
+3. make this an excersice, continue with this
+*/

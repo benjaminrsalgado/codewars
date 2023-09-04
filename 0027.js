d954797d2ae5c23e6234d628b3e6deb32d94ee2e
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-09-04 14:48:35 -0600

    i did a function constuctor and an array, as practice

--- 0027.js
+++ 0027.js
@@ -1,1 +1,12 @@
-//this is another example
+//this is an example make all Upper Casse
+const makeUpperCase = str => str.toUpperCase();
+
+// Practice Function Constructor 
+
+Function Animals (size1, color1, type1, age1)
+this size: size1
+this color: color1
+this type: type1
+this age: age1
+
+let bear = new Animals ('70cm','brown', 'Exotic', '10 years' )

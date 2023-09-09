c75bd370c88be1f8496c3b611a741638d7dd8f7c
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-09-08 19:47:19 -0600

    i did a function construction as excersice

---
+++ 0030.js
@@ -0,0 +1,9 @@
+// this is a function construction
+
+Function Animals (size1, color1, type1, age1)
+this size: size1
+this color: color1
+this type: type1
+this age: age1
+
+let bear = new Animals ('70cm','brown', 'Exotic', '10 years' )

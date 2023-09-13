8bb654188312c880ec9939fadc31ff3b5b32c904
Benjamin Rojo <benjaminrsalgado@gmail.com>
2023-09-13 15:37:39 -0600

    i did two codewars that i already did

--- 0032.js
+++ 0032.js
@@ -1,7 +1,7 @@
 /* Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).*/
 
 function countSheeps(arrayOfSheeps) {
-    return arrayOfSheeps.filter(Boolean).length;
+    return arrayOfSheeps.filter(boolean).length;
 }
 
 // arrayOfSheeps.filter(Boolean): Aquí, arrayOfSheeps es el array que pasaste como argumento a la función. 

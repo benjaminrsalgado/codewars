6a445bea705976959b339c9013f9a48bcdc4e174
Benjamin Salgado <benjaminrsalgado@gmail.com>
2023-09-25 15:05:08 -0600

    i add a comment explain about my codewars excersice

--- 0034.js
+++ 0034.js
@@ -3,3 +3,7 @@
 function opposite(number) {
     return -(number);
   }
+
+
+  /*
+  return -(number): Lo que hace es tomar el número pasado como argumento (number) y negarlo usando el operador de negación -. El operador - antes de number cambia el signo del número. Por ejemplo, si number es 5, la expresión -5 devolverá -5, y si number es -3, la expresión -(-3) devolverá 3*/

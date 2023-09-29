d1a63840fbbdbd480352f092aa95bfc38fe83f90
Benjamin Salgado <benjaminrsalgado@gmail.com>
2023-09-29 17:57:39 -0600

    i did a excersice codewars

---
+++ 0037.js
@@ -0,0 +1,12 @@
+// We need a function that can transform a number (integer) into a string.
+
+function numberToString(num) {
+    let cadena = num.toString()
+    return cadena
+}
+
+// let cadena es una variable que es creada para almacenar el valor de num convertido a string
+
+// num.toString() es una función que convierte el valor de num a string
+
+// return cadena es una función que devuelve el valor de cadena

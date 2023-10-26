37973d9330e9b5ab09b72728d7e9bbb7f8bae9ee
Benjamin Salgado <benjaminrsalgado@gmail.com>
2023-10-26 13:42:25 -0600

    i did a codewars excersice

--- 0041.js
+++ 0041.js
@@ -4,3 +4,4 @@ function makeNegative(num) {
     return -Math.abs(num);
 }
 
+// math.abs regresa un numero positivo si tu le pones -5 te va a dar 5. Pero en este caso como esta el -Math.abs con signo negativo si yo le pongo 10 va a dar -10

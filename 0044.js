d82fa3b2086262cd51c922aba3862a79a2d74acd
Benjamin Salgado <benjaminrsalgado@gmail.com>
2023-11-15 12:47:08 -0600

    i did some excersice

--- 0044.js
+++ 0044.js
@@ -45,3 +45,72 @@ if (pin.length === 4 || pin.length === 6) {
   return false;
 
 
+// Write a function that removes the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+  return x.replace(/\s/g, '');
+}
+
+/*
+El /\s/g es una expresión regular que encuentra todos los espacios en blanco (\s) en la cadena. El modificador g al final significa "global", lo que indica que debe buscar y reemplazar todas las instancias de espacios en blanco en toda la cadena.
+ */
+
+
+
+
+/*
+Write a function named setAlarm/set_alarm/set-alarm/setalarm (depending on language) which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm)
+*/
+
+function setAlarm(employed, vacation) {
+  if (employed && !vacation) {
+      return true; // Estás empleado y no estás de vacaciones, entonces debes poner la alarma.
+  } else {
+      return false; // Cualquier otro caso, no necesitas poner la alarma.
+  }
+}
+
+
+/*
+En esta variable !vacation verifica si la variable vacation es false. 
+*/
+
+
+
+/*
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+*/
+
+let name = "name"
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`
+}
+
+
+/*
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+*/
+
+function findAverage(array) {
+  if (array.length === 0) {
+    return 0; // Si el array está vacío, el promedio es 0
+  }
+  
+  // Suma de todos los elementos en el array
+  const sum = array.reduce((a, b) => a + b);
+  
+  // Dividir la suma por la cantidad de elementos para obtener el promedio
+  const average = sum / array.length;
+  
+  return average; // Devolver el promedio
+}
+
+//array.length proporciona el número de elementos en el array. Al dividir la suma total de los elementos sum
+

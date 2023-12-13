361bfeeda3de47cd04a782d84ace335b0005ce24
Benjamin Salgado <benjaminrsalgado@gmail.com>
2023-12-13 11:37:39 -0600

    i did a codewars excersices

---
+++ 0051.js
@@ -0,0 +1,16 @@
+//Multiply two parameters
+function multiply(a, b){
+    a * b
+  }
+
+//We need a function that can transform a number (integer) into a string.
+
+function numberToString(num) {
+    let cadena = num.toString()
+    return cadena
+}
+
+// Complete the solution so that it reverses the string passed into it.
+function solution(str){
+    return str.split('').reverse().join('');
+}

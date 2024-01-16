d40fae5299f0dbb6a93f27c3d6213f72639538f5
Benjamin Salgado <benjaminrsalgado@gmail.com>
2024-01-16 17:38:21 -0600

    i did excersices codewars in Java Script, every file it has a excersice

---
+++ 0052.js
@@ -0,0 +1,19 @@
+/*
+Multiply
+*/
+
+function multiply(a, b){
+   return a * b
+}
+
+/*
+You Can't count under pressure #1
+
+- Code as fast as you can! You need to double the integer and return it.
+*/
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    
+    return i;
+}

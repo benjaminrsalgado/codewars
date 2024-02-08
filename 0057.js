c7bbaff0ed6eda89e1ae8f3625c5f62f375195af
Benjamin Rojo <benjaminrsalgado@gmail.com>
2024-02-08 12:13:18 -0600

    i did some excersices

---
+++ 0057.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('li')addEvenetListener('click',change)
+
+Function change(){
+    this.Style.background = 'red'
+}
+
+
+document.querySelector('#button')addEventListener('click',web)
+
+function web(){
+    let nombre = document.querySelector('#nombre').value
+    let edad = document.querySelector('#edad').value
+    let ciudad = document.querySelector('#ciudad').value
+}
+

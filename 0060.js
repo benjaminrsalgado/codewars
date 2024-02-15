dfe71ab93aba0dd32bb77132ed663c188b196106
Benjamin Rojo <benjaminrsalgado@gmail.com>
2024-02-15 11:52:47 -0600

    i did excersises of js

---
+++ 0060.js
@@ -0,0 +1,9 @@
+function sumaCuadrados (num1, num2){
+    let cuadrado = num1 ** 2;
+    let cuadradoDos = num2 **2; 
+    let sumaCuadrados = cuadrado + cuadradoDos
+    return  sumaCuadrados
+}
+
+sumaCuadrados (10, 5)
+

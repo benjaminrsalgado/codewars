d5c3dd640684bc5c792bfc16ffcf6662e7092da1
Benjamin Rojo <benjaminrsalgado@gmail.com>
2024-03-19 09:57:19 -0600

    practice functions

---
+++ 0068.js
@@ -0,0 +1,16 @@
+// function normal
+
+function multipy (num){
+    let double = num * 2
+    return double
+
+}
+
+// Arrow function 
+
+calculateArea = (base, height) => {
+    let area = (base * height)/2
+    return area
+}
+calculateArea (20,50)
+
